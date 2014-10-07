9494b86f0884f7df0f868ff46aa15cb7780fe1c6
elliot woods <elliot@kimchiandchips.com>
2014-10-07 15:51:48 +0900

    add Board (working on it

---
+++ src/ofxDigitalEmulsion/Item/Board.cpp
@@ -0,0 +1,98 @@
+#include "Board.h"
+#include "../../../addons/ofxCvGui2/src/ofxCvGui.h"
+#include "ofxCvMin.h"
+#include "../Utils/Gui.h"
+
+using namespace ofxCvGui;
+
+namespace ofxDigitalEmulsion {
+	namespace Item {
+		//----------
+		Board::Board() {
+			this->boardType.set("Board Type", 0, 0, 1);
+			this->sizeX.set("Size X", 9.0f, 2.0f, 20.0f);
+			this->sizeY.set("Size Y", 5.0f, 2.0f, 20.0f);
+			this->spacing.set("Spacing [m]", 0.025f, 0.001f, 1.0f);
+			this->updatePreviewMesh();
+		}
+
+		//----------
+		string Board::getTypeName() const {
+			return "Board";
+		}
+
+		//----------
+		ofxCvGui::PanelPtr Board::getView() {
+			auto view = MAKE(ofxCvGui::Panels::World);
+			view->onDrawWorld += [this](ofCamera &) {
+				this->previewMesh.draw();
+			};
+			view->setGridEnabled(false);
+#ifdef USE_OFXGRABCAM
+			view->getCamera().setCursorDraw(true, this->spacing / 5.0f);
+#endif
+
+			auto & camera = view->getCamera();
+			auto distance = this->spacing * MAX(this->sizeX, this->sizeY);
+			camera.setPosition(-distance, distance, -distance);
+			camera.lookAt(ofVec3f());
+
+			return view;
+		}
+
+		//----------
+		void Board::serialize(Json::Value & json) {
+			Utils::Serializable::serialize(this->boardType, json);
+			Utils::Serializable::serialize(this->sizeX, json);
+			Utils::Serializable::serialize(this->sizeY, json);
+			Utils::Serializable::serialize(this->spacing, json);
+		}
+
+		//----------
+		void Board::deserialize(const Json::Value & json) {
+			Utils::Serializable::deserialize(this->boardType, json);
+			Utils::Serializable::deserialize(this->sizeX, json);
+			Utils::Serializable::deserialize(this->sizeY, json);
+			Utils::Serializable::deserialize(this->spacing, json);
+
+			this->updatePreviewMesh();
+		}
+
+		//----------
+		cv::Size Board::getSize() const {
+			return cv::Size(this->sizeX, this->sizeY);
+		}
+
+		//----------
+		vector<cv::Point3f> Board::getObjectPoints() const {
+			return ofxCv::makeCheckerboardPoints(this->getSize(), this->spacing);
+		}
+
+		//----------
+		void Board::populateInspector2(ElementGroupPtr inspector) {
+			auto sliderCallback = [this](ofParameter<float> &) {
+				this->updatePreviewMesh();
+			};
+
+			auto typeChooser = Widgets::MultipleChoice::make("Board type");
+			typeChooser->addOption("Checkerboard");
+			typeChooser->addOption("Circles");
+			inspector->add(typeChooser);
+
+			Utils::Gui::addIntSlider(this->sizeX, inspector)->onValueChange += sliderCallback;
+			Utils::Gui::addIntSlider(this->sizeY, inspector)->onValueChange += sliderCallback;
+			inspector->add(Widgets::Title::make("NB : Checkerboard size is\n counted by number of\n inner corners", Widgets::Title::Level::H3));
+			inspector->add(Widgets::Spacer::make());
+
+			auto spacingSlider = Widgets::Slider::make(this->spacing);
+			spacingSlider->onValueChange += sliderCallback;
+			inspector->add(spacingSlider);
+		}
+
+		//----------
+		void Board::updatePreviewMesh() {
+			if (this->)
+			//this->previewMesh = ofxCv::makeBoardMesh(this->getSize(), this->spacing);
+		}
+	}
+}
---
+++ src/ofxDigitalEmulsion/Item/Board.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Base.h"
+#include "ofxCvMin.h"
+
+namespace ofxDigitalEmulsion {
+	namespace Item {
+		class Board : public Base {
+		public:
+			Board();
+			string getTypeName() const override;
+			ofxCvGui::PanelPtr getView();
+
+			void serialize(Json::Value &) override;
+			void deserialize(const Json::Value &) override;
+
+			cv::Size getSize() const;
+			vector<cv::Point3f> getObjectPoints() const;
+		protected:
+			void populateInspector2(ofxCvGui::ElementGroupPtr) override;
+			void updatePreviewMesh();
+
+			ofParameter<int> boardType;
+			ofParameter<float> sizeX, sizeY;
+			ofParameter<float> spacing;
+			ofMesh previewMesh;
+		};
+	}
+}
