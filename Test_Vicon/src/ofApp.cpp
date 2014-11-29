2de429885ab61a4695f168c217a5c689789cfd43
Elliot Woods <elliot-web@kimchiandchips.com>
2014-11-29 07:49:08 +0100

    add blank Vicon project

---
+++ Test_Vicon/src/ofApp.cpp
@@ -0,0 +1,89 @@
+#include "ofApp.h"
+
+using namespace ofxAssets;
+
+//--------------------------------------------------------------
+void ofApp::setup2(){
+	ofSetWindowTitle("Digital Emulsion Toolkit v0.1");
+	ofSetCircleResolution(120);
+	
+	AssetRegister.addAddon("ofxDigitalEmulsion");
+	splashScreen.init(image("ofxDigitalEmulsion::SplashScreen"));
+	splashScreen.begin(0.0f);
+	
+	//auto cameraNode = MAKE(Item::Camera);
+	//this->world.add(cameraNode);
+
+	//auto boardNode = MAKE(Item::Board);
+	//this->world.add(boardNode);
+
+	//auto cameraCalibrateNode = MAKE(Procedure::Calibrate::CameraIntrinsics);
+	//this->world.add(cameraCalibrateNode);
+
+	auto patchInstance = MAKE(ofxDigitalEmulsion::Graph::Editor::Patch);
+	this->world.add(patchInstance);
+
+	this->gui.init();
+	this->world.init(this->gui.getController());
+	this->world.loadAll();
+	
+	this->splashScreen.end();
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	this->world.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	if (ofGetFrameNum() == 2) {
+		this->setup2();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
