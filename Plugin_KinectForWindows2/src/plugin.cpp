fd063bd2340e8d546aa71f2f60bd453fc2b4c6dc
Elliot Woods <elliot@kimchiandchips.com>
2015-12-04 12:35:39 +0900

    update KinectForWindows2 plugin

--- Plugin_KinectForWindows2/src/plugin.cpp
+++ Plugin_KinectForWindows2/src/plugin.cpp
@@ -4,8 +4,8 @@
 #include "ofxRulr/Nodes/Procedure/Calibrate/CameraFromKinectV2.h"
 #include "ofxRulr/Nodes/Procedure/Calibrate/ProjectorFromKinectV2.h"
 
-OFXPLUGIN_INIT_BEGIN(ofxRulr::Nodes::Base)
-	OFXPLUGIN_REGISTER(ofxRulr::Nodes::Item::KinectV2);
-	OFXPLUGIN_REGISTER(ofxRulr::Nodes::Procedure::Calibrate::CameraFromKinectV2);
-	OFXPLUGIN_REGISTER(ofxRulr::Nodes::Procedure::Calibrate::ProjectorFromKinectV2);
-OFXPLUGIN_INIT_END
+OFXPLUGIN_PLUGIN_MODULES_BEGIN(ofxRulr::Nodes::Base)
+	OFXPLUGIN_PLUGIN_REGISTER_MODULE(ofxRulr::Nodes::Item::KinectV2);
+	OFXPLUGIN_PLUGIN_REGISTER_MODULE(ofxRulr::Nodes::Procedure::Calibrate::CameraFromKinectV2);
+	OFXPLUGIN_PLUGIN_REGISTER_MODULE(ofxRulr::Nodes::Procedure::Calibrate::ProjectorFromKinectV2);
+OFXPLUGIN_PLUGIN_MODULES_END
