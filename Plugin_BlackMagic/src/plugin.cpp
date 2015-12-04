51d78bdf6c1c80f9af35fd7535b9c51082aa067e
Elliot Woods <elliot@kimchiandchips.com>
2015-12-04 12:35:24 +0900

    BlackMagic plugin

---
+++ Plugin_BlackMagic/src/plugin.cpp
@@ -0,0 +1,6 @@
+#include "ofxMachineVision/Device/DeckLink.h"
+#include "ofxPlugin.h"
+
+OFXPLUGIN_PLUGIN_MODULES_BEGIN(ofxMachineVision::Device::Base)
+	OFXPLUGIN_PLUGIN_REGISTER_MODULE(ofxMachineVision::Device::DeckLink);
+OFXPLUGIN_PLUGIN_MODULES_END
