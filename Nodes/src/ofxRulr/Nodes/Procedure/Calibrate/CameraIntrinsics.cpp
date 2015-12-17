10a4357e148950053d6931a247314a028d553ef9
Elliot Woods <elliot@kimchiandchips.com>
2015-12-17 15:27:18 +0900

    ditto

--- Nodes/src/ofxRulr/Nodes/Procedure/Calibrate/CameraIntrinsics.cpp
+++ Nodes/src/ofxRulr/Nodes/Procedure/Calibrate/CameraIntrinsics.cpp
@@ -38,44 +38,47 @@ namespace ofxRulr {
 					this->view->onDraw += [this](DrawArguments & drawArgs) {
 						auto camera = this->getInput<Item::Camera>();
 						if (camera) {
-							auto grabber = camera->getGrabber();
-							if (this->grayscale.isAllocated()) {
-								this->grayscale.draw(drawArgs.localBounds);
-							}
+							if (this->getRunFinderEnabled()) {
+								auto grabber = camera->getGrabber();
+								if (this->grayscale.isAllocated()) {
+									this->grayscale.draw(drawArgs.localBounds);
+								}
 
-							ofPushMatrix();
-							{
-								ofScale(drawArgs.localBounds.getWidth() / camera->getWidth(), drawArgs.localBounds.getHeight() / camera->getHeight());
-
-								//draw current corners
-								ofxCv::drawCorners(this->currentCorners);
-
-								//draw past corners
-								ofPushStyle();
+								ofPushMatrix();
 								{
-									ofFill();
-									ofSetLineWidth(0.0f);
-									int boardIndex = 0;
-									ofColor boardColor(200, 100, 100);
-									for (auto & board : this->accumulatedCorners) {
-										boardColor.setHue(boardIndex++ * 30 % 360);
-										ofSetColor(boardColor);
-										for (auto & corner : board) {
-											ofCircle(corner, 3.0f);
+									//scale view to camera coordinates
+									auto grabber = camera->getGrabber();
+									auto cameraWidth = grabber->getWidth();
+									auto cameraHeight = grabber->getHeight();
+									ofScale(drawArgs.localBounds.getWidth() / cameraWidth, drawArgs.localBounds.getHeight() / cameraHeight);
+
+									//draw current corners
+									ofxCv::drawCorners(this->currentCorners);
+
+									//draw past corners
+									ofPushStyle();
+									{
+										ofFill();
+										ofSetLineWidth(0.0f);
+										int boardIndex = 0;
+										ofColor boardColor(200, 100, 100);
+										for (auto & board : this->accumulatedCorners) {
+											boardColor.setHue(boardIndex++ * 30 % 360);
+											ofSetColor(boardColor);
+											for (auto & corner : board) {
+												ofCircle(corner, 3.0f);
+											}
 										}
 									}
+									ofPopStyle();
 								}
-								ofPopStyle();
+								ofPopMatrix();
 							}
-							ofPopMatrix();
-
-							if (!this->isBeingInspected()) {
-								ofPushStyle(); 
+							else {
+								ofPushStyle();
 								{
 									ofSetColor(255, 100);
-									if (grabber->getDeviceSpecification().supports(ofxMachineVision::Feature::Feature_FreeRun)) {
-										ofxCvGui::Utils::drawText("Select node to enable board finder...", drawArgs.localBounds);
-									}
+									ofxCvGui::Utils::drawText("Select node to enable board finder...", drawArgs.localBounds);
 								}
 								ofPopStyle();
 							}
@@ -100,16 +103,14 @@ namespace ofxRulr {
 
 				//----------
 				void CameraIntrinsics::update() {
-					if (this->isBeingInspected()) {
+					if (this->getRunFinderEnabled()) {
 						auto camera = this->getInput<Item::Camera>();
 						if (camera) {
 							auto grabber = camera->getGrabber();
-							if (grabber->getDeviceSpecification().supports(ofxMachineVision::Feature::Feature_FreeRun)) {
-								try {
-									this->findBoard();
-								}
-								RULR_CATCH_ALL_TO_ERROR
+							try {
+								this->findBoard();
 							}
+							RULR_CATCH_ALL_TO_ERROR
 						}
 					}
 				}
@@ -139,6 +140,27 @@ namespace ofxRulr {
 						this->accumulatedCorners.push_back(board);
 					}
 					Utils::Serializable::deserialize(this->error, json);
+				}
+
+				//----------
+				bool CameraIntrinsics::getRunFinderEnabled() const {
+					if (this->isBeingInspected()) {
+						//find when we're selected
+						return true;
+					}
+					else {
+						auto camera = this->getInput<Item::Camera>();
+						if (camera) {
+							auto grabber = camera->getGrabber();
+							if (!grabber->getDeviceSpecification().supports(ofxMachineVision::Feature::Feature_FreeRun)) {
+								//find when the camera is a single shot camera
+								return true;
+							}
+						}
+					}
+
+					//if nothing was good, then return false
+					return false;
 				}
 
 				//----------
