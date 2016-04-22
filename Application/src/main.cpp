abe060fa818f19e64ebee233ea64480ecf13bd37
Elie Zananiri <prisonerjohn@gmail.com>
2016-04-22 16:53:08 +0900

    Fixed GL version.

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -4,7 +4,7 @@
 //========================================================================
 int main() {
 	ofGLFWWindowSettings windowSettings;
-	windowSettings.setGLVersion(2, 0);
+	windowSettings.setGLVersion(RULR_GL_VERSION_MAJOR, RULR_GL_VERSION_MINOR);
 	windowSettings.width = 1920;
 	windowSettings.height = 1080;
 	windowSettings.depthBits = 32;
